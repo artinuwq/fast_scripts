11dd2c33d805dff0f6b563689934b9989bcb1560
artinuwq <artinuwq@inbox.ru>
2025-06-03 19:00:25 +0300

    new file:   test_1.py"

---
+++ OOP😱/test_1.py
@@ -0,0 +1,25 @@
+class Cat: 
+    #Поля переменные класса
+    name = None
+    age = None
+    IsHappy = None
+    some = {}
+#Тут мы общие характеристики добавили
+
+cat1 = Cat()
+cat1.name = "MARSIK"
+cat1.age = 3
+cat1.IsHappy = True
+
+# Это у нас полиморфизм?
+
+cat2 = Cat()
+cat2.name = "Снежок"
+cat2.age = 2
+cat2.IsHappy = False
+
+
+
+#print(cat1.name)
+#print(cat2.name)
+
