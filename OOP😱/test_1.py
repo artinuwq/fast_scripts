90f7c721340c1a1347ea55f5c7b03a4f3d15de83
artinuwq <artinuwq@inbox.ru>
2025-06-03 22:02:31 +0300

    modified:   "test_1.py" 	new file:   "test_2.py"

--- OOP😱/test_1.py
+++ OOP😱/test_1.py
@@ -4,22 +4,34 @@ class Cat:
     age = None
     IsHappy = None
     some = {}
+
+    def __init__(self, name = None, age = None, IsHappy = None): #<--- Конструктор по умолчанию
+        self.name = name
+        self.age = age
+        self.IsHappy = IsHappy
+        self.get_data()
+
+
+    def set_data(self, name = None, age = None, IsHappy = None): #<--- здесь всегда должен быть self Метод=функция
+        self.name = name
+        self.age = age
+        self.IsHappy = IsHappy
+    
+    def get_data(self):
+        print(self.name, "age", self.age, ". Happe:", self.IsHappy)
+
+        
 #Тут мы общие характеристики добавили
 
-cat1 = Cat()
-cat1.name = "MARSIK"
-cat1.age = 3
-cat1.IsHappy = True
 
 # Это у нас полиморфизм?
+cat1 = Cat()
+cat1.set_data("Jonh")
 
-cat2 = Cat()
-cat2.name = "Снежок"
-cat2.age = 2
-cat2.IsHappy = False
+cat2 = Cat("snesok", 2, False)
 
+print("ИТОГ:")
 
-
-#print(cat1.name)
-#print(cat2.name)
+cat1.get_data()
+cat2.get_data()
 
